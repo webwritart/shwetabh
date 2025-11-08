f81aa904d7f34c1b2bbcf5d76d32e707d0e1a4bc
Shwetabh <webwritat@gmail.com>
2025-11-08 19:34:43 +0530

    Add Captcha to contact form

---
+++ operations/miscellaneous.py
@@ -0,0 +1,121 @@
+import os
+
+from PIL import Image
+from datetime import datetime
+import random
+from captcha.image import ImageCaptcha
+import base64
+
+
+def log(text, category):
+    today = str(datetime.today())
+    if category == 'error':
+        text = f"        <p style='line-height:0.1;'> {today} - <span style='color:red;'>{text}</span> </p>\n"
+    elif category == 'success':
+        text = f"        <p style='line-height:0.1;'> {today} - <span style='color:green;'>{text}</span> </p>\n"
+    elif category == 'routine':
+        text = f"        <p style='line-height:0.1;'> {today} - <span style='color:yellow;'>{text}</span> </p>\n"
+    elif category == 'none':
+        text = f"        <p style='line-height:0.1;'> {today} - <span style='color:white;'>{text}</span> </p>\n"
+    with open("./routes/templates/manager/log.html", "r") as f:
+        contents = f.readlines()
+        lines = len(contents)
+        index = lines - 3
+        contents.insert(index, text)
+    with open("./routes/templates/manager/log.html", "w") as f:
+        contents = "".join(contents)
+        f.write(contents)
+        f.close()
+    print("logged into log.txt successfully")
+
+
+def delete_log(date):
+    try:
+        with open("./routes/templates/manager/log.html", "r") as f:
+            contents = f.readlines()
+        with open("./routes/templates/manager/log.html", "w") as f:
+            for line in contents:
+                if "<p" in line:
+                    log_date = log_date.replace("<p style='line-height:0.1;'> ", "")
+                    log_date = log_date.replace(" - <span style='color:red;'>", "")
+                    log_date = log_date.replace("</span> <p>", "")
+                    log_date = log_date.split(" ")
+                    log_date = log_date[0]
+                    if not log_date <= date:
+                        f.write(line)
+                else:
+                    f.write(line)
+        f.close()
+        log("Successfully deleted log", "success")
+    except Exception as e:
+        log("Error in deleting log", "error")
+
+
+def resize_image(input_folder, size_f_t, output_folder):
+    global new_height, output_filepath
+    if input_folder[-1] != '/':
+        input_folder = input_folder + '/'
+    all_images = os.listdir(input_folder)
+    if output_folder[-1] != '/':
+        output_folder = output_folder + '/'
+
+    for a in all_images:
+        image_path = input_folder + a
+
+        if a.endswith(".jpg") or a.endswith(".png"):
+
+            fixed_full_height = 900
+            fixed_thumbnail_height = 200
+
+            if size_f_t == 'f':
+                new_height = fixed_full_height
+            elif size_f_t == 't':
+                new_height = fixed_thumbnail_height
+            else:
+                pass
+            try:
+                image = Image.open(image_path)
+                width = image.width
+                height = image.height
+                filename = image.filename
+                new_name = filename.split('.')
+                file_name = new_name[0].split('\\')[-1]
+                file_name_filled_space = file_name.replace(' ', '-')
+                # new_filename = file_name_filled_space + '.jpg'
+                # extension = new_filename.pop()
+
+                if size_f_t == 'f':
+                    new_filename = f"{file_name_filled_space}-f.jpg"
+                    output_filepath = output_folder + new_filename
+                elif size_f_t == 't':
+                    new_filename = f"{file_name_filled_space}-t.jpg"
+                    output_filepath = output_folder + new_filename
+
+                ratio = (new_height / float(height))
+                new_width = int(float(width * ratio))
+
+                image = image.resize((new_width, new_height))
+                image = image.convert('RGB')
+                if not os.path.exists(output_folder):
+                    os.mkdir(output_folder)
+                image.save(output_filepath)
+
+            except Exception as e:
+                pass
+        else:
+            pass
+
+
+def generate_captcha():
+    captcha_num = str(random.randrange(100000, 999999))
+    img = ImageCaptcha()
+    captcha_io = img.generate(captcha_num)
+    binary_data = captcha_io.getvalue()
+    encoded_data_bytes = base64.b64encode(binary_data)
+    encoded_string = encoded_data_bytes.decode('utf-8')
+    captcha_uri = f"data:image/png;base64, {encoded_string}"
+    return captcha_num, captcha_uri
+
+
+
+
--- routes/main.py
+++ routes/main.py
@@ -3,6 +3,8 @@ import os
 from flask import Blueprint, render_template, request, flash, session, url_for, redirect
 from extensions import current_year
 from operations.messenger import send_email
+from operations.miscellaneous import generate_captcha
+from datetime import datetime
 
 
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
@@ -33,22 +35,35 @@ def concept_artworks():
 
 @main.route('/contact', methods=['GET', 'POST'])
 def contact():
+    captcha_value, captcha_uri = generate_captcha()
+    session['captcha_value'] = captcha_value
+    session['url'] = request.url
 
+    return render_template('contact.html', captcha=captcha_uri, captcha_value=captcha_value)
+
+
+@main.route('/captcha_verification', methods=['GET', 'POST'])
+def captcha_verification():
     if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        email2 = request.form.get('email2')
-        msg = request.form.get('message')
-        message = f'{msg}\n\n\nSENDER DETAILS:\nName: {name}\nEmail: {email}\n'
-
-        success_msg = (f'Dear {name},\n\nThanks for sending me a message.\nI will get back to you as soon as possible.\nHave a nice day! '
-                       f':)\n\n\nShwetabh Suman\nConcept Artist & Illustrator\nNew Delhi, India')
-
-        if email == email2:
-            send_email('IMPORTANT!! - Main Portfolio', ['shwetabhartist@gmail.com'], email, message, '', '')
-            send_email('MESSAGE SENT - Shwetabh Suman', [email], 'shwetabhartist@gmail.com', success_msg, '', '')
-            flash('Message sent successfully!', 'success')
+        captcha_value = session.get('captcha_value')
+        if request.form.get('captcha') != captcha_value:
+            spam_report = f'{datetime.now()} -- Captcha Fail -- \n'
+            with open('spam_report.txt', 'a') as f:
+                f.write(spam_report)
+            flash("Wrong Captcha!", "error")
         else:
-            flash("The email doesn't match!", "error")
-            return redirect(request.url)
-    return render_template('contact.html', current_year=current_year)
+            name = request.form.get('name')
+            email = request.form.get('email')
+            email2 = request.form.get('email2')
+            msg = request.form.get('message')
+            if email == email2:
+                message = f'{msg}\n\n\nSENDER DETAILS:\nName: {name}\nEmail: {email}\n'
+
+                success_msg = (f'Dear {name},\n\nThanks for sending me a message.\nI will get back to you as soon as possible. '
+                           f':)\n\n\nShwetabh Suman\nConcept Artist & Illustrator\nNew Delhi, India')
+                send_email('IMPORTANT!! - All Portfolio', ['shwetabhartist@gmail.com'], email, message, '', '')
+                send_email('MESSAGE SENT - Shwetabh Suman', [email], 'shwetabhartist@gmail.com', success_msg, '', '')
+                flash('Message sent successfully!', 'success')
+            else:
+                flash("The email doesn't match!", "error")
+    return redirect(session.get('url'))
